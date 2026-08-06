d6fad7b15ff75721dad3cc8e456a52d77c4dfc0c
Hoputer819 <programmer0819@gmail.com>
2026-08-07 00:25:10 +0900

    부호있고 없는 정수간 형변환

---
+++ SignedUnsignedConversion/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Formats.Asn1;
+
+namespace SignedUnsignedConversion
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 500;
+            Console.WriteLine(a);
+
+            uint b = (uint)a;
+            Console.WriteLine(b);
+
+            int x = -30;
+            Console.WriteLine(x);
+
+            uint y = (uint)x;
+            Console.WriteLine(y);
+
+        }
+    }
+}
