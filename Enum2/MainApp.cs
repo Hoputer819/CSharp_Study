0943c0cb247f22243efe22a22bed29a3dfc1de3b
Hoputer819 <programmer0819@gmail.com>
2026-08-07 01:22:15 +0900

    열거2

---
+++ Enum2/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enum2
+{
+    internal class MainApp
+    {
+        enum DialogResult {YES,NO,CANCEL,CONFIRM,OK}
+        static void Main(string[] args)
+        {
+            DialogResult result = DialogResult.YES;
+
+            Console.WriteLine(result == DialogResult.YES);
+            Console.WriteLine(result == DialogResult.NO);
+            Console.WriteLine(result == DialogResult.CANCEL);
+            Console.WriteLine(result == DialogResult.CONFIRM);
+            Console.WriteLine(result == DialogResult.OK);
+        }
+    }
+}
