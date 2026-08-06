f61738ee7214dad52099fc77b31702be8db7f77e
Hoputer819 <programmer0819@gmail.com>
2026-08-07 01:38:13 +0900

    Enum3

---
+++ Enum3/MainApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enum3
+{
+    internal class MainApp
+    {
+        enum DialogResult { YES = 10, NO, CANCEL, CONFIRM = 50, OK }
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCEL);
+            Console.WriteLine((int)DialogResult.CONFIRM);
+            Console.WriteLine((int)DialogResult.OK);
+        }
+    }
+}
