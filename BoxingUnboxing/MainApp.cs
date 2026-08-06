41416aa6b462fa94a87045c85903d29e02a8e45b
Hoputer819 <programmer0819@gmail.com>
2026-08-06 15:48:41 +0900

    박싱,언박싱

---
+++ BoxingUnboxing/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BoxingUnboxing
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            object b = (object)a;
+            int c = (int)b;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            double x = 3.1414213;
+            object y = x;
+            double z = (double)y;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
