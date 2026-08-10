3ffdf53208f7761d58c769c78bcc593ff064ee0f
Hoputer819 <programmer0819@gmail.com>
2026-08-11 02:51:35 +0900

    Format 서식 맞추기

---
+++ StringFormatBasic/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+
+namespace StringFormatBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fmt = "{0,-20}{1,-15}{2,30}";
+
+            WriteLine(fmt, "Publisher", "Author", "Title");
+            WriteLine(fmt, "Marvel", "Stan Lee", "Iron Man");
+            WriteLine(fmt, "Hanbit", "Sanhyun Park", "This is C#");
+            WriteLine(fmt, "Prentice Hall", "K&R", "The C Programming Language");
+        }
+    }
+}
