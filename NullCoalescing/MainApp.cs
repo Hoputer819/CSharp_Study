9d1e73630461b3b62cd0a087ff4b220acf6e8513
Hoputer819 <programmer0819@gmail.com>
2026-08-20 00:26:44 +0900

    null 병합 연산자

---
+++ NullCoalescing/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NullCoalescing
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int? num = null;
+            Console.WriteLine($"{num ?? 0}");
+
+            num = 99;
+            Console.WriteLine($"{num ?? 0}");
+
+            string str = null;
+            Console.WriteLine($"{str ?? "Default"}");
+
+            str = "Specific";
+            Console.WriteLine($"{str ?? "Default"}");
+        }
+    }
+}
