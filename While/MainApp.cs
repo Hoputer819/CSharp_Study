f5585a4f80c4775cfedf8333aca4f4dc91f1490b
Hoputer819 <programmer0819@gmail.com>
2026-08-24 22:34:09 +0900

    while문

---
+++ While/MainApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace While
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            while(i > 0)
+            {
+                Console.WriteLine($"i : {i--}");
+            }
+        }
+    }
+}
